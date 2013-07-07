dfbffb4f8853e45c8d90a7351818fc47ef7d66e9
Willgolden <willgolden86@yahoo.com>
2013-07-07 02:44:14 +0000

    test

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World 2!');
-var fname = fs.readFileSync('./index.html');
+var fname = fs.readFileSync('./index.html','utf8');
 response.send(fname);
 });
 
